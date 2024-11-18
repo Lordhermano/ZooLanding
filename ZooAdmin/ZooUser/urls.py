5a83c0d362b1250ac235688cc2272bb9df9ad54b
Lordhermano <160242165@student.rnngroup.ac.uk>
2024-11-18 12:15:42 +0000

    NavBar

--- ZooAdmin/ZooUser/urls.py
+++ ZooAdmin/ZooUser/urls.py
@@ -2,4 +2,5 @@ from django.urls import path,include
 
 from . import views
 
-urlpatterns = [path('',views.home,name=''),]
+urlpatterns = [path('',views.home,name=''),
+    path('hotel',views.hotel,name='hotel')]
--- ZooAdmin/ZooUser/views.py
+++ ZooAdmin/ZooUser/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'base.html')
+
+def hotel(request):
+    return render(request,'hotel.html')    
